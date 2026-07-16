4e3fa4fb9c563f01b74679d5d2fb018c263002b4
macowen1616 <Cra20019@byui.edu>
2026-07-16 17:01:36 +0000

    Deploying to website from @ macowen1616/WDD331_Practice@429ddf6593b85f4fda34be3d903c32bbca0e81ae 🚀

--- js/theme-preference.js
+++ js/theme-preference.js
@@ -64,3 +64,15 @@
         }
     });
 })();
+
+// Scroll progress bar
+window.addEventListener("scroll", () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const scrollPercent = (scrollTop / scrollHeight) * 100;
+
+    document.getElementById("progress-bar").style.width = scrollPercent + "%";
+});
